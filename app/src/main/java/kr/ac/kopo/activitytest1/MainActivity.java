fe12400529a9b749c18c4551d1500ff1df0574ec
smcmfmf <hk0831bae@gmail.com>
2025-06-23 14:59:40 +0900

    라디오 버튼을 사용한 엑티비티 이동하기

--- app/src/main/java/kr/ac/kopo/activitytest1/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/activitytest1/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    RadioButton rbSecond, rbThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +28,26 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        rg = findViewById(R.id.rg);
+        rbSecond = findViewById(R.id.rb_second);
+        rbSecond.setChecked(true); // 처음부터 선택되어 있도록
+
         Button btnMain = findViewById(R.id.btn_main);
         btnMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), SecondActivity.class); // 인텐트 객체 생성
+                Intent intent = null; // 인텐트 객체 생성
+                if(rg.getCheckedRadioButtonId() == R.id.btn_second)
+                {
+                    intent = new Intent(getApplicationContext(), SecondActivity.class);
+                }
+                else
+                {
+                    intent = new Intent(getApplicationContext(), ThirdActivity.class);
+                }
                 startActivity(intent);
             }
         });
+
     }
 }
